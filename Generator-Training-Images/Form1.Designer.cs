7b678fc9070bc7f7cafa54aa150b6166faa4ca8c
FaetterP <faettterp@gmail.com>
2021-10-16 00:59:14 +0300

    Добавлен интерфейс

--- Generator-Training-Images/Form1.Designer.cs
+++ Generator-Training-Images/Form1.Designer.cs
@@ -29,13 +29,216 @@ namespace Generator_Training_Images
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.BigPictureBox = new System.Windows.Forms.PictureBox();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.DownRightButton = new System.Windows.Forms.Button();
+            this.DownLeftButton = new System.Windows.Forms.Button();
+            this.UpRightButton = new System.Windows.Forms.Button();
+            this.UpLeftButton = new System.Windows.Forms.Button();
+            this.WhiteButton = new System.Windows.Forms.Button();
+            this.BlackButton = new System.Windows.Forms.Button();
+            this.SkipButton = new System.Windows.Forms.Button();
+            this.SmallPictureBox = new System.Windows.Forms.PictureBox();
+            this.ChooseFolderButton = new System.Windows.Forms.Button();
+            this.PathLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BigPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SmallPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            // 
+            // BigPictureBox
+            // 
+            this.BigPictureBox.Location = new System.Drawing.Point(189, 54);
+            this.BigPictureBox.Name = "BigPictureBox";
+            this.BigPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.BigPictureBox.TabIndex = 1;
+            this.BigPictureBox.TabStop = false;
+            // 
+            // UpButton
+            // 
+            this.UpButton.Location = new System.Drawing.Point(463, 41);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(75, 23);
+            this.UpButton.TabIndex = 2;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            // 
+            // DownButton
+            // 
+            this.DownButton.Location = new System.Drawing.Point(565, 41);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(75, 23);
+            this.DownButton.TabIndex = 3;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Location = new System.Drawing.Point(463, 81);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(75, 23);
+            this.LeftButton.TabIndex = 4;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(565, 81);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(75, 23);
+            this.RightButton.TabIndex = 5;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            // 
+            // DownRightButton
+            // 
+            this.DownRightButton.Location = new System.Drawing.Point(565, 162);
+            this.DownRightButton.Name = "DownRightButton";
+            this.DownRightButton.Size = new System.Drawing.Size(75, 23);
+            this.DownRightButton.TabIndex = 9;
+            this.DownRightButton.Text = "DownRight";
+            this.DownRightButton.UseVisualStyleBackColor = true;
+            // 
+            // DownLeftButton
+            // 
+            this.DownLeftButton.Location = new System.Drawing.Point(463, 162);
+            this.DownLeftButton.Name = "DownLeftButton";
+            this.DownLeftButton.Size = new System.Drawing.Size(75, 23);
+            this.DownLeftButton.TabIndex = 8;
+            this.DownLeftButton.Text = "DownLeft";
+            this.DownLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // UpRightButton
+            // 
+            this.UpRightButton.Location = new System.Drawing.Point(565, 122);
+            this.UpRightButton.Name = "UpRightButton";
+            this.UpRightButton.Size = new System.Drawing.Size(75, 23);
+            this.UpRightButton.TabIndex = 7;
+            this.UpRightButton.Text = "UpRight";
+            this.UpRightButton.UseVisualStyleBackColor = true;
+            // 
+            // UpLeftButton
+            // 
+            this.UpLeftButton.Location = new System.Drawing.Point(463, 122);
+            this.UpLeftButton.Name = "UpLeftButton";
+            this.UpLeftButton.Size = new System.Drawing.Size(75, 23);
+            this.UpLeftButton.TabIndex = 6;
+            this.UpLeftButton.Text = "UpLeft";
+            this.UpLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // WhiteButton
+            // 
+            this.WhiteButton.Location = new System.Drawing.Point(565, 203);
+            this.WhiteButton.Name = "WhiteButton";
+            this.WhiteButton.Size = new System.Drawing.Size(75, 23);
+            this.WhiteButton.TabIndex = 11;
+            this.WhiteButton.Text = "White";
+            this.WhiteButton.UseVisualStyleBackColor = true;
+            // 
+            // BlackButton
+            // 
+            this.BlackButton.Location = new System.Drawing.Point(463, 203);
+            this.BlackButton.Name = "BlackButton";
+            this.BlackButton.Size = new System.Drawing.Size(75, 23);
+            this.BlackButton.TabIndex = 10;
+            this.BlackButton.Text = "Black";
+            this.BlackButton.UseVisualStyleBackColor = true;
+            // 
+            // SkipButton
+            // 
+            this.SkipButton.Location = new System.Drawing.Point(669, 122);
+            this.SkipButton.Name = "SkipButton";
+            this.SkipButton.Size = new System.Drawing.Size(75, 23);
+            this.SkipButton.TabIndex = 12;
+            this.SkipButton.Text = "Skip";
+            this.SkipButton.UseVisualStyleBackColor = true;
+            // 
+            // SmallPictureBox
+            // 
+            this.SmallPictureBox.Location = new System.Drawing.Point(12, 54);
+            this.SmallPictureBox.Name = "SmallPictureBox";
+            this.SmallPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.SmallPictureBox.TabIndex = 13;
+            this.SmallPictureBox.TabStop = false;
+            // 
+            // ChooseFolderButton
+            // 
+            this.ChooseFolderButton.Location = new System.Drawing.Point(12, 214);
+            this.ChooseFolderButton.Name = "ChooseFolderButton";
+            this.ChooseFolderButton.Size = new System.Drawing.Size(100, 42);
+            this.ChooseFolderButton.TabIndex = 14;
+            this.ChooseFolderButton.Text = "Choose save folder";
+            this.ChooseFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.AutoSize = true;
+            this.PathLabel.Location = new System.Drawing.Point(12, 259);
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(41, 17);
+            this.PathLabel.TabIndex = 15;
+            this.PathLabel.Text = "Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PathLabel);
+            this.Controls.Add(this.ChooseFolderButton);
+            this.Controls.Add(this.SkipButton);
+            this.Controls.Add(this.WhiteButton);
+            this.Controls.Add(this.BlackButton);
+            this.Controls.Add(this.DownRightButton);
+            this.Controls.Add(this.DownLeftButton);
+            this.Controls.Add(this.UpRightButton);
+            this.Controls.Add(this.UpLeftButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.SmallPictureBox);
+            this.Controls.Add(this.BigPictureBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.BigPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SmallPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.PictureBox BigPictureBox;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button DownRightButton;
+        private System.Windows.Forms.Button DownLeftButton;
+        private System.Windows.Forms.Button UpRightButton;
+        private System.Windows.Forms.Button UpLeftButton;
+        private System.Windows.Forms.Button WhiteButton;
+        private System.Windows.Forms.Button BlackButton;
+        private System.Windows.Forms.Button SkipButton;
+        private System.Windows.Forms.PictureBox SmallPictureBox;
+        private System.Windows.Forms.Button ChooseFolderButton;
+        private System.Windows.Forms.Label PathLabel;
     }
 }
 
